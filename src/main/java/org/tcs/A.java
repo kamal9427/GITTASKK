420c2d48057aee4240b01bdc4f0feacfb1f2afae
kamal9427 <kamalakannan9427@gmail.com>
2022-11-07 19:03:20 +0530

    two

--- src/main/java/org/tcs/A.java
+++ src/main/java/org/tcs/A.java
@@ -2,16 +2,29 @@ package org.tcs;
 
 public class A {
 	
-	private void method1() {
+	public void method1() {
 		System.out.println("1");
 	}
 	
-	private void method2() {
+	public void method2() {
 		System.out.println("2");
 	}
 	
-	private void method3() {
+	public void method3() {
+		System.out.println("3");
+	}
+	
+	public void method4() {
+		System.out.println("1");
+	}
+	
+	public void method5() {
+		System.out.println("2");
+	}
+	
+	public void method6() {
 		System.out.println("3");
 	}
 
+	
 }
